062f76e91e622a8aa700e520502c8b8991845cd6
Chamara D Rodrigo <108047725+ChamaraRodrigo@users.noreply.github.com>
2024-09-27 13:32:15 +0530

    basic
    1st disply

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 // import './index.css';  // Optional, for adding global styles
-// import App from './App';
+import App from './app';
 
 ReactDOM.render(
   <React.StrictMode>
